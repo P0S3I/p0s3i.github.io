631d1a5098b13f83c5a48670a300c4ddf6713959
P0S3I <99417762+P0S3I@users.noreply.github.com>
2022-03-15 10:02:02 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+window.sr =ScrollReveal
+
+    sr.reval('.principal-page', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reval('.container', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
